aaba2044d3fda4aa44d553339def8fc7aa36b4ef
EC2 Default User <ec2-user@ip-172-31-1-21.ap-northeast-1.compute.internal>
2023-05-31 12:54:40 +0000

    start4-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,27 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+  
+  def create
+    @post_image = PostImage.new(post_image_params)
   end
   
   def index
   end
     
   def show  
+  end
+  
+  def update
+    
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    
+    redirect_to book_path(@book.id) 
+    else
+    render "edit"
+    end
   end
 end
