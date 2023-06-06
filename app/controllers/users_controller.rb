8be39c7c7b81778f8f73beb1abcf8930cb2d7244
EC2 Default User <ec2-user@ip-172-31-1-21.ap-northeast-1.compute.internal>
2023-06-06 12:36:11 +0000

    start4-11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
+  
+  def create
+    @post_image = PostImage.new(post_image_params)
+    if @book.save
+      flash[:notice] = "successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alret] = "error"
+      @books = Book.all
+      render "index"
+    end
+  end
+  
   def edit
     @user = User
     
